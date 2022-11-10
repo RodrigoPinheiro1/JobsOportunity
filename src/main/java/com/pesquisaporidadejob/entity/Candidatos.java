6eb9fa8087ed19f5f97c76f9c3af7c9e2be29f13
RodrigoPinheiro1 <rodrigo.pinheiro@atos.net>
2022-11-10 20:55:57 -0300

    Adicionado Entidades e relacionamentos

---
+++ src/main/java/com/pesquisaporidadejob/entity/Candidatos.java
@@ -0,0 +1,32 @@
+package com.pesquisaporidadejob.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Candidatos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id = UUID.randomUUID();
+
+    private String nome;
+
+    private String vaga;
+
+    private LocalDate dtAberturaVaga;
+
+
+
+}
