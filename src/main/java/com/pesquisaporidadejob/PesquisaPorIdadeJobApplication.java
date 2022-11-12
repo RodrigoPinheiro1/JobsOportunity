184c104c5067f4155024599fa5f1b7bb8cf780a7
RodrigoPinheiro1 <rodrigo.pinheiro@atos.net>
2022-11-11 21:43:00 -0300

    projeto

---
+++ src/main/java/com/pesquisaporidadejob/PesquisaPorIdadeJobApplication.java
@@ -0,0 +1,13 @@
+package com.pesquisaporidadejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisaPorIdadeJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PesquisaPorIdadeJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pesquisaporidadejob/PesquisaPorIdadeJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesquisaporidadejob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisaPorIdadeJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
