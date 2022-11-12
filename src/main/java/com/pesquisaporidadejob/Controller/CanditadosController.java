bf470d921704f6953709c9970bf8d56797a852e9
RodrigoPinheiro1 <rodrigo.pinheiro@atos.net>
2022-11-11 21:42:25 -0300

    adicionado consulta e cadastro de vagas e candidatos

--- src/main/java/com/pesquisaporidadejob/Controller/CanditadosController.java
+++ src/main/java/com/pesquisaporidadejob/Controller/CanditadosController.java
@@ -1,16 +1,54 @@
 package com.pesquisaporidadejob.Controller;
 
 
+import com.pesquisaporidadejob.Dto.CandidatosDto;
+import com.pesquisaporidadejob.Dto.VagasDto;
 import com.pesquisaporidadejob.service.CandidatosService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.transaction.Transactional;
+import java.net.URI;
+import java.time.LocalDate;
 
 @RestController
-@RequestMapping("candidados")
+@RequestMapping("/candidatos")
 public class CanditadosController {
 
 
+    @Autowired
     private CandidatosService service;
+
+
+    @GetMapping
+    public Page<CandidatosDto> paginacaoComIdade(@RequestParam(required = false)
+                                                 @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fromAge,
+                                                 @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                                 @RequestParam(required = false) LocalDate toAge,
+                                                 @PageableDefault(sort = "id", direction = Sort.Direction.DESC,
+                                                         page = 0, size = 10) Pageable pageable) {
+
+
+        return service.paginacaoIdade(pageable, fromAge, toAge);
+    }
+
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<VagasDto> cadastrar(@RequestBody VagasDto dto, UriComponentsBuilder uriComponentsBuilder) {
+
+        VagasDto cadastrar = service.cadastrar(dto);
+
+        URI uri = uriComponentsBuilder.path("/candidatos").buildAndExpand(cadastrar.getId()).toUri();
+        return ResponseEntity.created(uri).body(cadastrar);
+    }
 
 
 }
--- src/main/java/com/pesquisaporidadejob/service/CandidatosService.java
+++ src/main/java/com/pesquisaporidadejob/service/CandidatosService.java
@@ -1,10 +1,61 @@
 package com.pesquisaporidadejob.service;
 
+import com.pesquisaporidadejob.Dto.CandidatosDto;
+import com.pesquisaporidadejob.Dto.VagasDto;
+import com.pesquisaporidadejob.entity.Candidatos;
+import com.pesquisaporidadejob.entity.Vagas;
+import com.pesquisaporidadejob.repository.CandidatosRepository;
+import com.pesquisaporidadejob.repository.VagasRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-@Service
-public class CandidatosService {
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
+@Service
+public class CandidatosService implements CanditadoInterface {
+
+
+    @Autowired
+    private CandidatosRepository candidatosRepository;
+
+
+    @Autowired
+    private VagasRepository vagasRepository;
+
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public Page<CandidatosDto> paginacaoIdade(Pageable pageable, LocalDate fromAge, LocalDate toAge) {
+
+
+        return candidatosRepository.findByDataNascimentoBetween(fromAge, toAge, pageable)
+                .map(candidatos -> modelMapper.map(candidatos, CandidatosDto.class));
+
+    }
+    public void converter (LocalDate fromAge , LocalDate toAge){
+
+        fromAge.getYear();
+        int year = toAge.getYear();
+
+    }
+
+    @Override
+    public VagasDto cadastrar(VagasDto dto) {
+
+        Vagas vagas = modelMapper.map(dto, Vagas.class);
+
+        vagasRepository.save(vagas);
+
+        return modelMapper.map(vagas, VagasDto.class);
+
+    }
 
 
 }
